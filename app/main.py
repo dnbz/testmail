fd4998cbc27b55d997b28e16fc9a005a46d46bbf
Leo Leo <leo@meniay.com>
2023-10-06 15:49:05 +0500

    improve exception handling in the endpoints
    Handle known errors and return 400 with the error message. Throw 500 for unknown errors and only log them

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Depends, HTTPException
 
 from app.dependencies import get_email_sender
 from app.models import EmailSendRequest, EmailSendResponse
-from app.sender import SmtpEmailSender
+from app.sender import SmtpEmailSender, EmailSendException
 
 app = FastAPI()
 
@@ -16,9 +16,13 @@ async def send_email(request: EmailSendRequest,
 
     try:
         sender.send_mail(to=request.to, subject=request.subject, message=request.message)
+    except EmailSendException as e:
+        logging.error(f"Error sending mail: {e}")
+        raise HTTPException(status_code=400, detail=f"Email could not be sent: {e}")
+    # Catch all other exceptions but don't expose them to the user
     except Exception as e:
-        logging.error(f"Couldn't send mail: {e}")
-        raise HTTPException(status_code=200, detail="Email could not be sent")
+        logging.error(f"An unexpected error occurred: {e}")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred")
 
     return EmailSendResponse(
         message="Email sent successfully"
